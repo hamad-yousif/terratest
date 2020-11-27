3d22185094a7b23743f2ea539784e4198319d03c
Cloud User <ec2-user@ip-172-31-21-79.ca-central-1.compute.internal>
2020-11-27 00:20:54 +0000

    First commit

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "eu-central-1"
+}
+terraform {
+  backend "s3" {
+    bucket = "hamad-secret-app"
+    key    = "hamad-remote-key"
+    region = "ca-central-1"
+  }
+}
+resource "aws_s3_bucket" "s3Bucket" {
+  bucket = "hamadtestwebsite"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+  "Id": "MakePublic",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::hamadtestwebsite/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+  }
+}
